b21c103af8d5723cc3d21bdf8ae8dfac82eb9c3f
Kai Wingfield <kaiwingfield17@gmail.com>
2023-11-15 16:05:36 -0800

    improved tail function

--- tail.js
+++ tail.js
@@ -1,3 +1,15 @@
+// Assert Equal Function
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉💖🎉 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫😒🚫 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Tail function
+
 const tail = function(array) {
   let tailArray = array.slice(1);
   return tailArray;
@@ -7,3 +19,5 @@ const tail = function(array) {
 let test = [1, 2, 3, 4, 5];
 console.log(tail(test));
 console.log(test);
+console.log(tail([]))
+assertEqual(test.length, 5)
