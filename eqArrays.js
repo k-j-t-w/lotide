698ca2767ecb06d49b0882818943b52c8f4f420d
Kai Wingfield <kaiwingfield17@gmail.com>
2023-11-16 16:26:27 -0800

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉💖🎉 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫😒🚫 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2){
+for (let i = 0; i < arr1.length; i++) {
+  if(arr1[i] !== arr2[i]){
+    return false;
+  }
+  
+}
+  return true;
+}
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
