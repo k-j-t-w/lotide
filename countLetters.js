a6c4d55b0255ad65225f56169fbec03e2da76bec
Kai Wingfield <kaiwingfield17@gmail.com>
2023-11-21 13:56:01 -0800

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉💖🎉 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫😒🚫 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(stringToCount) {
+  let result = {};
+  let stringArray = stringToCount.split("");
+
+  for (let letter of stringArray) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+
+
+  return result;
+};
+
+const results1 = countLetters("one two three four");
+console.log(countLetters("Helloh"));
+assertEqual(results1["o"], 3);
+assertEqual(results1.t, 2);
+console.log(results1);
