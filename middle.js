212b6741d1d69c0593aac3ee74f175c6838f0e31
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-07 13:34:54 -0800

    seperated and refactored code for middle.js

--- middle.js
+++ middle.js
@@ -1,34 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉💖🎉 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫😒🚫 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✔✔✔ Assertion passed: ${arr1} === ${arr2}.`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion failed: ${arr1} !== ${arr2}.`);
-  }
-};
-
-
 const middle = function(array) {
   let middleElement = [];
   if (array.length <= 2) {
@@ -44,9 +13,4 @@ const middle = function(array) {
 };
 
 
-console.log(middle([1, 2]));
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3 ,4]), [2, 3]);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
