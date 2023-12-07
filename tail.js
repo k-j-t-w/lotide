59ae6cc551ed409e9938d18822fe4e11872a8e46
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-07 13:15:20 -0800

    seperated test code from tail.js

--- tail.js
+++ tail.js
@@ -1,25 +1,6 @@
-// Assert Equal Function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉💖🎉 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫😒🚫 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Tail function
-
 const tail = function(array) {
   let tailArray = array.slice(1);
   return tailArray;
 };
 
-
-let test = [1, 2, 3, 4, 5];
-console.log(tail(test));
-assertEqual(tail(test)[0], test[1])
-console.log(test);
-assertEqual(tail([]).length, 0)
-assertEqual(tail(["test"]).length, 0)
-assertEqual(test.length, 5)
+module.exports = tail
---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+const tail = require('../tail')
+
+let test = [1, 2, 3, 4, 5];
+assertEqual(tail(test)[0], test[1])
+assertEqual(tail([]).length, 0)
+assertEqual(tail(["test"]).length, 0)
+assertEqual(test.length, 5)
